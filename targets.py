9630b0dfd0e192000261faa89a7340a66222c41d
p58797557 <p58797557@gmail.com>
2026-04-14 15:30:32 +0530

    Add files via upload

---
+++ targets.py
@@ -0,0 +1,15 @@
+# ============================================================
+# targets.py
+# Local targets only (Firebase removed)
+# ============================================================
+
+# ================= CONFIG =================
+# Add target usernames here
+TARGET_USERS = [ 
+    "skbauddin7",
+    "amankashi54"
+
+]
+
+if not TARGET_USERS:
+    raise Exception("ERROR: No targets found in targets.py")
